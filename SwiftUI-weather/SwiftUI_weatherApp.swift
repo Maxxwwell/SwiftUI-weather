58d571fd285bf1ed1a86076c063113e708c1d50c
Maxwell <76234406+Maxxwwell@users.noreply.github.com>
2022-05-02 02:05:06 +0000

    Initial Commit

---
+++ SwiftUI-weather/SwiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherApp.swift
+//  SwiftUI-weather
+//
+//  Created by Maxwell on 02/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
